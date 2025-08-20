a0b65f634d91c49727c2dcd6bd03e9ef44e33d99
TeaTimeJack <amitjack14@gmail.com>
2025-08-20 14:07:39 +0300

    finnished the daily challeng

--- week-1/day-1/daily-challenge/daily-challenge.py
+++ week-1/day-1/daily-challenge/daily-challenge.py
@@ -1,52 +1,29 @@
-#Exercise 1 : Hello World
-print("Hello World\n"*4)
+import random
 
-#Exercise 2 : Some Math
-print((99^3)*8)
-
-#Exercise 3 : What is the output ?
-print(5 < 3) #False
-print(3 == 3) #True
-print(3 == "3") #False
-#print("3" > 3)  #type error
-print("Hello" == "hello")  #False
-
-#Exercise 4 : Your computer brand
-computer_brand = "ASUS"
-print(f"I have a {computer_brand} computer")
-
-#Exercise 5 : Your information
-name = "Amit"
-age = 34
-shoe_size = 43
-info = f"My name is {name} im {age} years old and my shoe size is {shoe_size}"
-print(info)
-
-#Exercise 6 : A & B
-a = 5
-b = 2
-if a > b:
-    print("Hello world")
-
-#Exercise 7 : Odd or Even
-user_number = int(input("type a number "))
-if user_number % 2 == 0:
-    print("Even")
+user_string = input("type anything you like with 10 characters")
+if  len(user_string) == 10:
+    print("Perfect string")
+elif len(user_string) < 10:
+    print("String not long enough.")
 else:
-    print("odd")
+    print("String too long.")
 
-#Exercise 8 : What’s your name ?
-name_input = input("whats your name? ")
-my_name = "Amit"
+print(user_string[0])
+print(user_string[-1])
 
-if name_input == my_name:
-    print("i cant belive we have the SAME NAME!")
-else:
-    print("we have difrent names")
+pyramid_string = ""
+for char in user_string:
+    pyramid_string += char
+    print(pyramid_string)
 
-#Exercise 9 : Tall enough to ride a roller coaster
-users_height = int(input("What is your heigt? "))
-if users_height > 145:
-    print("you are tall enough to ride this roller coaster")
-else:
-     print("you need to grow some more to ride this roller coaster")   
+user_list =[]
+for char in user_string:
+    user_list.append(char)
+print(user_list)
+random.shuffle(user_list)
+print(user_list)
+
+new_string=""
+for i in user_list:
+    new_string += i
+print(new_string)
