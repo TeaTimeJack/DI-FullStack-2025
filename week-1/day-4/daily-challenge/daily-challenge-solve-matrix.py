8c9806ace084f42a36ad73ef8781b306ad16c974
TeaTimeJack <amitjack14@gmail.com>
2025-08-25 21:57:05 +0300

    changed function to have argumentts and not change global verubals

--- week-1/day-4/daily-challenge/daily-challenge-solve-matrix.py
+++ week-1/day-4/daily-challenge/daily-challenge-solve-matrix.py
@@ -23,10 +23,10 @@ $a
 #     return list_2d
 
 #Step 1: Transforming the String into a 2D List - this does not use hard coding
-def transform_to_2d():
+def transform_to_2d(arg):
     list_2d = []
     row_2d = []
-    for char in MATRIX_STR:
+    for char in arg:
         if char == "\n":
             if row_2d:
                 list_2d.append(row_2d)
@@ -37,21 +37,20 @@ def transform_to_2d():
     return list_2d
 
 # Step 2: Processing Columns
-def iterate_columns():
-    global matrix
+def iterate_columns(arg):
     column_text = []
     column_num = 0
-    while column_num < len(matrix[0]):
-        for row in matrix:
+    while column_num < len(arg[0]):
+        for row in arg:
             column_text.append(row[column_num])    
         column_num += 1
-    matrix = column_text    
+    return column_text    
 
 #Step 3: Filtering Alpha Characters + Step 4: Replacing Symbols with Spaces
-def filter_alpha():
+def filter_alpha(arg):
     temp_string = ''
     sign_counter = 0
-    for item in matrix:
+    for item in arg:
         if item.isalpha():
             sign_counter = 0
             temp_string += item
@@ -63,6 +62,8 @@ def filter_alpha():
     return temp_string
 
 # Step 5: Constructing the Secret Message
-matrix = transform_to_2d()
-iterate_columns()
-print(filter_alpha())
+def main_function():
+    matrix = transform_to_2d(MATRIX_STR)
+    matrix = iterate_columns(matrix)
+    print(filter_alpha(matrix))
+main_function()    
