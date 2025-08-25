2675bc697e4b9c70e26c5e7efcf3b67e29e2b414
TeaTimeJack <amitjack14@gmail.com>
2025-08-25 21:46:01 +0300

    changed all hard coding numbers to values in the data so it will work for eny lenght of matrix

--- week-1/day-4/daily-challenge/daily-challenge-solve-matrix.py
+++ week-1/day-4/daily-challenge/daily-challenge-solve-matrix.py
@@ -8,17 +8,32 @@ sM#
 $a 
 #t%'''  
 
-#Step 1: Transforming the String into a 2D List
+
+#Step 1: Transforming the String into a 2D List - this uses hard coding
+# def transform_to_2d():
+#     list_2d = []
+#     row_2d = []
+#     for char in MATRIX_STR:
+#         if char == "\n":
+#             continue
+#         else: row_2d.append(char)
+#         if len(row_2d) == 3:
+#             list_2d.append(row_2d)
+#             row_2d = []
+#     return list_2d
+
+#Step 1: Transforming the String into a 2D List - this does not use hard coding
 def transform_to_2d():
     list_2d = []
     row_2d = []
     for char in MATRIX_STR:
         if char == "\n":
+            if row_2d:
+                list_2d.append(row_2d)
+                row_2d = []  
             continue
         else: row_2d.append(char)
-        if len(row_2d) == 3:
-            list_2d.append(row_2d)
-            row_2d = []
+    list_2d.append(row_2d)    
     return list_2d
 
 # Step 2: Processing Columns
@@ -26,7 +41,7 @@ def iterate_columns():
     global matrix
     column_text = []
     column_num = 0
-    while column_num < 3:
+    while column_num < len(matrix[0]):
         for row in matrix:
             column_text.append(row[column_num])    
         column_num += 1
@@ -42,12 +57,12 @@ def filter_alpha():
             temp_string += item
         else:
             sign_counter+=1
-            if sign_counter == 2:
+            if sign_counter == 2: #hard coded because of the instrutions gave to me
                 temp_string += " "
                 sign_counter = 0
     return temp_string
 
-#Step 5: Constructing the Secret Message
+# Step 5: Constructing the Secret Message
 matrix = transform_to_2d()
 iterate_columns()
 print(filter_alpha())
