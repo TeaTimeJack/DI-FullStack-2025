2cd4ecd2e19ea1eb736c3a976739223261639468
TeaTimeJack <amitjack14@gmail.com>
2025-08-27 22:31:57 +0300

    finnished exercies xp on week 2 day 2

--- week-2/day-2/Exercise/exercise-XP.py
+++ week-2/day-2/Exercise/exercise-XP.py
@@ -1,152 +1,145 @@
-#Exercise 1: Favorite Numbers
-my_fav_numbers = {2, 4, 6}
-print(my_fav_numbers)
+import random
 
-my_fav_numbers.add(8)
-my_fav_numbers.add(10)
-print(my_fav_numbers)
+#Exercise 1: Pets
+class Pets():
+    def __init__(self, animals):
+        self.animals = animals
 
-my_fav_numbers.remove(10)
-print(my_fav_numbers)
+    def walk(self):
+        for animal in self.animals:
+            print(animal.walk())
 
-friend_fav_numbers = {1,3,5}
-our_fav_numbers = my_fav_numbers.union(friend_fav_numbers)
-print(our_fav_numbers)
+class Cat():
+    is_lazy = True
 
-#Exercise 2: Tuple
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
+    def walk(self):
+        return f'{self.name} is just walking around'
 
+class Bengal(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
 
-#Exercise 3: List Manipulation
-basket = ["Banana", "Apples", "Oranges", "Blueberries"]
-basket.remove("Banana")
-basket.remove("Blueberries")
-basket.append("Kiwi")
-basket.insert(0,"Apples")
-print(basket.count("Apples"))
-basket.clear()
-print(basket)
+class Chartreux(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
 
+#Step 1: Create the Siamese Class
+class Siamese(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+    
+cat1 = Bengal("BEN", 3)
+cat2 = Chartreux("CHAR", 4)
+cat3 = Siamese("SI", 2)
+all_cats = [cat1, cat2, cat3]
 
-#Exercise 4: Floats
-
-list_of_int_and_floats = []
-my_float = 1
-
-while len(list_of_int_and_floats) != 8:
-        my_float = my_float + 0.5
-
-        list_of_int_and_floats.append(my_float)
-print(list_of_int_and_floats)
-
-#Exercise 5: For Loop
-
-numbers=[]
-for i in range(1,21):
-        print(i)
-        numbers.append(i)
-
-for i in numbers:
-        if numbers.index(i) %2 == 0:
-            print(i)
-
-#Exercise 6: While Loop
-
-while True:
-        user_input=input("enter name: ")
-        if user_input == "amit":
-                break
-
-#Exercise 7: Favorite Fruits
-
-fruits_input = input("enter your favorite fruits: ")
-list_of_fruits = []
-fruit = ""
-for char in fruits_input:
-    if char != " ":
-        fruit += char
-    else:
-        list_of_fruits.append(fruit)
-        fruit = ""
-list_of_fruits.append(fruit)
-print(list_of_fruits)
-
-name_fruit = input("name any fruit: ")
-
-if  name_fruit in list_of_fruits:
-    print("You chose one of your favorite fruits! Enjoy!")
-else:
-    print("You chose a new fruit. I hope you enjoy it!")
-
-#Exercise 8: Pizza Toppings
-
-pizza_toppings = []
-while True:
-    user_topping = input("Choose a topping to add to your pizza:   (to stop type 'quit')")
-    if user_topping == "quit":
-          break
-    pizza_toppings.append(user_topping)
-    print(f"Adding {user_topping} to your pizza.")
-print(f"your toppings are {pizza_toppings}")
-pizza_price = 10
-for i in pizza_toppings:
-      pizza_price += 2.5
-print(f"Your pizza price is ${pizza_price}")
-
-#Exercise 9: Cinemax Tickets
-
-list_of_ages = []
-age = ''
-user_ages = input("Type the ages of each person who wants to buy a movie ticket: ")
-for char in user_ages:
-    if char != " ":
-        age += char
-    else:
-        list_of_ages.append(int(age))
-        age = ""
-list_of_ages.append(int(age))
-print(list_of_ages)
-total_price = 0
-for person_age in list_of_ages:
-    if person_age > 12:
-        total_price += 15
-    elif person_age >= 3 and person_age <= 12:
-        total_price += 10
-print(f"Your total ticket price is ${total_price}")
-
-#bonus
-list_of_ages2 = []
-age2 = ''
-user_ages2 = input("Type the ages of each person who wants to buy a movie ticket: ")
-for char in user_ages2:
-    if char != " ":
-        age2 += char
-    else:
-        list_of_ages2.append(int(age2))
-        age2 = ""
-list_of_ages2.append(int(age2))
-print(list_of_ages2)
-
-final_attendees = []
-for teen_age in list_of_ages2:
-    if teen_age < 16 or teen_age > 21:
-        print(f"the person age of {teen_age} is not allowed to wathch this movie")
-    else:
-        final_attendees.append(teen_age)
-
-print(final_attendees)
+sara_pets = Pets(all_cats)
+sara_pets.walk()
 
 
-#Exercise 10: Sandwich Orders
-sandwich_orders = ["Tuna", "Pastrami", "Avocado", "Pastrami", "Egg", "Chicken", "Pastrami"]
-# print(sandwich_orders)
-for order in sandwich_orders:
-    if order == "Pastrami":
-        sandwich_orders.remove(order)
-# print(sandwich_orders)
+#Exercise 2: Dogs
+class Dog:
+    def __init__(self,name, age:int, weight:int):
+        self.name = name
+        self.age = age
+        self.weight = weight
+        
+    def bark(self):
+        return self.name + " is barking"
+    
+    def run_speed(self):
+        return self.weight / self.age * 10
+    
+    def fight(self, other_dog):
+        if (self.run_speed()*self.weight) > (other_dog.run_speed()*other_dog.weight):
+            return f"{self.name} Won the Fight!"
+        elif (self.run_speed()*self.weight) < (other_dog.run_speed()*other_dog.weight):
+            return f"{self.name} Won the Fight!"
+        else: return "ITS A TIE!"
 
-finished_sandwiches = []
-for preparing in sandwich_orders:
-    print(f"I made your {preparing} sandwich.")
-    finished_sandwiches.append(preparing)
-print(f"list of finished sandwiches {finished_sandwiches}")
+dog1 = Dog("REX", 5, 20)
+dog2 = Dog("JOH", 6, 18)
+dog3 = Dog("MIKE", 4, 24)
+
+print(dog1.bark())
+print(dog2.run_speed())
+print(dog1.fight(dog2))
+
+#Exercise 3: Dogs Domesticated
+#type this in a diffrent file and import the class 'Dogs'
+class PetDog(Dog):
+    def __init__(self, name, age, weight, trained = False):
+        super().__init__(name, age, weight)
+    
+    def train(self):
+        print(self.bark())
+        self.trained = True
+
+    def play(self, *args):
+        string_to_print = f"{self.name} "
+        for dog in args:
+            string_to_print += f"and {dog.name} "
+        string_to_print += "all play together"
+        print(string_to_print)
+        
+
+    def do_a_trick(self):
+        if self.trained:
+            tricks = ["does a barrel roll", "stands on his back legs", "shakes your hand", "plays dead"]
+            random_index = random.randint(0,3)
+            print(tricks[random_index])
+
+pet_dog1 = PetDog("Mika", 8, 10, False)
+pet_dog1.train()
+pet_dog1.play(dog1,dog2,dog3)
+pet_dog1.do_a_trick()
+
+#Exercise 4: Family and Person Classes
+#Step 1: Create the Person Class
+class Person:
+    def __init__(self, first_name, age:int, last_name=""):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = int(age)
+
+    def is_18(self):
+        if self.age >= 18:
+            return True
+        else: return False
+
+#Step 2: Create the Family Class
+class Family:
+    def __init__(self, last_name):
+        self.last_name = last_name
+        self.members = []
+
+    def born(self, first_name, age):
+        new_person = Person(first_name, age, self.last_name)
+        self.members.append(new_person)
+
+    def check_majority(self, first_name):
+        for member in self.members:
+            if member.first_name == first_name:
+                if member.is_18():
+                    print("You are over 18, your parents Jane and John accept that you will go out with your friends")
+                else: print("Sorry, you are not allowed to go out with your friends.")
+    
+    def family_presentation(self):
+        print(f"The {self.last_name} Family")
+        for member in self.members:
+            print(f"{member.first_name}, {member.age} years old")
+
+my_family = Family("Jackson")
+my_family.born("Amit", 35)
+my_family.born("Lital", 34)
+my_family.born("Tohar", 6)
+my_family.born("Yuval", 4)
+my_family.born("Shahar", 1)
+
+my_family.check_majority("Lital")
+my_family.check_majority("Tohar")
+my_family.family_presentation()
