c2eeb4244af2d19882d847d61cde3837167f8b6a
TeaTimeJack <amitjack14@gmail.com>
2025-08-31 16:34:56 +0300

    finnishe the dalily challeng week 2 day 3

--- week-2/day-3/Exercises/Exercises-xp.py
+++ week-2/day-3/Exercises/Exercises-xp.py
@@ -17,98 +17,103 @@ class Currency:
     #Your code starts HERE
     def __str__(self):
         return f"{self.amount} {self.currency}"
+    
     def __int__(self):
         return int(self.amount)
+    
     def __repr__(self):
         return f"{self.__dict__}"
+    
     def __add__(self, other):
             if type(other) == int:
                 temp = self.amount + other
                 return temp
             else: 
                 if self.currency == other.currency:
-                    temp = self.__int__()+ other.amount.__int__()
+                    temp = self.amount + other.amount
                     return temp
                 else:
                     print( TypeError(f"Cannot add between Currency type {self.currency} and {other.currency}"))
+
     def __iadd__(self, other):
         if type(other) == int:
-            temp = self.__add__(other)
+            temp = self.amount + other
             self.amount = temp
-            return self.__int__()
+            return self.amount
         else: 
             temp = self.amount + other.amount
             self.amount = temp
+            return self.amount
 
 c1 = Currency('dollar', 5)
 c2 = Currency('dollar', 10)
 c3 = Currency('shekel', 1)
 c4 = Currency('shekel', 10)
 
-# #the comment is the expected output
-# print(c1)
-# # '5 dollars'
-# print(int(c1))
-# # 5
-# print(repr(c1))
-# # '5 dollars'
-# print(c1 + 5)
-# # 10
-# print(c1 + c2)
-# # 15
-# print(c1) 
-# # 5 dollars
-# c1 += 5
-# print(c1)
-# # 10 dollars
-# c1 += c2
-# print(c1)
-# # 20 dollars
-# print(c1 + c3)
-# # TypeError: Cannot add between Currency type <dollar> and <shekel>
+#the comment is the expected output
+print(c1)
+# '5 dollars'
+print(int(c1))
+# 5
+print(repr(c1))
+# '5 dollars'
+print(c1 + 5)
+# 10
+print(c1 + c2)
+# 15
+print(c1) 
+# 5 dollars
+c1 += 5
+print(c1)
+# 10 dollars
+c1 += c2
+print(c1)
+# 20 dollars
+print(c1 + c3)
+# TypeError: Cannot add between Currency type <dollar> and <shekel>
 
 
 
-## Exercise 3: String module
+# ## Exercise 3: String module
 
-all_letters = string.ascii_lowercase
-random_string =""
-lenght_rnd_string = 5
-for i in range(lenght_rnd_string):
-    index = random.randint(0,len(all_letters))
-    letter = all_letters[index]
-    random_string += letter
-print(random_string)
+# all_letters = string.ascii_lowercase
+# random_string =""
+# lenght_rnd_string = 5
+# for i in range(lenght_rnd_string):
+#     index = random.randint(0,len(all_letters))
+#     letter = all_letters[index]
+#     random_string += letter
+# print(random_string)
 
-##Exercise 4: Current Date
-today = datetime.date.today()
-print(today)
+# ##Exercise 4: Current Date
+# today = datetime.date.today()
+# print(today)
 
-#Exercise 5: Amount of time left until January 1st
-the_start_of_year = datetime.date(today.year+1,1,1)
-days_till_start = 365 - int(today.strftime('%j'))
-print(days_till_start)
+# #Exercise 5: Amount of time left until January 1st
+# the_start_of_year = datetime.date(today.year+1,1,1)
+# days_till_start = 365 - int(today.strftime('%j'))
+# print(days_till_start)
 
-##Exercise 6: Birthday and minutes
+# ##Exercise 6: Birthday and minutes
 
-def miniuts_alive(year, month, day):
-    user_birthday = datetime.datetime(year, month, day)
-    today = datetime.datetime.today()
-    time_diff = today - user_birthday
-    print(math.floor(time_diff.total_seconds()/60))
-miniuts_alive(1990,9,24)
+# def miniuts_alive(year, month, day):
+#     user_birthday = datetime.datetime(year, month, day)
+#     today = datetime.datetime.today()
+#     time_diff = today - user_birthday
+#     print(math.floor(time_diff.total_seconds()/60))
+# miniuts_alive(1990,9,24)
 
-##Exercise 7: Faker Module
+# ##Exercise 7: Faker Module
 
-user_list = []
-def add_users(num):
-    for i in range(num):
-        fake = Faker()
-        user_dict = {}
-        user_dict["name"] = fake.name()
-        user_dict["address"] = fake.address()
-        user_dict["language_code"] = fake.language_code()
-        user_list.append(user_dict)
+# user_list = []
+# def add_users(num):
+#     for i in range(num):
+#         fake = Faker()
+#         user_dict = {}
+#         user_dict["name"] = fake.name()
+#         user_dict["address"] = fake.address()
+#         user_dict["language_code"] = fake.language_code()
+#         user_list.append(user_dict)
 
-add_users(2)
-print(user_list)
+# add_users(2)
+# print(user_list)
---
+++ week-2/day-3/daily-challenge/Daily-Challenge-circle.py
@@ -0,0 +1,55 @@
+
+import math
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = self.format_radius(radius)
+
+    # @classmethod
+    # def form_radius(cls, diameter):
+
+    @staticmethod
+    def format_radius(radius):
+        return round(radius,2)
+
+    @property
+    def area(self):
+        return round(3.14*self.radius**2) 
+    
+    def __str__(self):
+        return f"Circle Radius: {self.radius} Area: {self.area}"
+    
+    def __add__(self,other):
+        sum_area = self.area + other.area
+        sum_radius = math.sqrt(sum_area/3.14)
+        # print(f"Circle Radius: {sum_radius} Area: {sum_area}")
+        return Circle(sum_radius)
+    
+    def __lt__(self, other):
+        return self.area < other.area
+    
+    def __eq__(self, other):
+        return self.area == other.area
+    
+    
+c1 = Circle(5)
+print(c1)
+c2 = Circle(2)
+print(c2)
+c3 = c1 + c2
+print(c3)
+c4 = Circle(2)
+
+print(c1>c2)
+print(c1==c2)
+print(c4==c2)
+
+circles_list = [c1, c2, c3, c4]
+def print_list(list):
+    for circle in list:
+        print(circle)
+print_list(circles_list)
+print("#####")
+circles_list.sort()
+print_list(circles_list)
+
