5e08b9394712e08d17cdf3bcbb87cfb1ff99152b
TeaTimeJack <amitjack14@gmail.com>
2025-09-01 21:23:35 +0300

    finnished mini project anagrams

--- week-2/day-5-mini-projct/Exercise/anagram_checker.py
+++ week-2/day-5-mini-projct/Exercise/anagram_checker.py
@@ -1,9 +1,54 @@
-file_path = "/week-2/day-5-mini-projct/Exercise/sowpods.txt"
 
-with open(file_path, 'r') as file:
-    file_lines = file.readlines()
-    print(file_lines)
+class AnagramChecker:
+    def __init__(self, user_word):
+        self.list_of_words = self.make_list_from_txt_file()
+        self.word = self.make_word_capitalised(user_word)
 
-# class AnagramChecker:
-#     def __init__(self):
-#         self.list_of_words = 
+    @staticmethod
+    def make_list_from_txt_file():
+        file_path = "week-2/day-5-mini-projct/Exercise/sowpods.txt"
+        text_file_list=[]
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+        for line in lines:
+            text_file_list.append(line.strip())
+        return text_file_list
+    
+    @staticmethod
+    def make_word_capitalised(word):
+        cap_word = word.upper()
+        return cap_word
+
+    def is_valid_word(self):
+        if self.word in self.list_of_words:
+            return True
+        else: return False
+
+    def is_anagram(self, word2):
+        if len(self.word) == len(word2):
+            for letter in self.word:
+                if letter not in word2:
+                    return False
+            for letter in word2:
+                if letter not in self.word:
+                    return False    
+            return True 
+        else: return False
+
+    def get_anagrams(self):
+        if self.is_valid_word():
+            list_of_anagrams = []
+            for word in self.list_of_words:
+                if word == self.word:
+                    continue
+                if self.is_anagram(word):
+                    list_of_anagrams.append(word.lower())
+            return list_of_anagrams
+        else:
+            return None
+
+# ana1 =AnagramChecker("kiss")
+# print(len(ana1.list_of_words))
+# print(ana1.is_valid_word())
+# print(ana1.get_anagrams())
+
---
+++ week-2/day-5-mini-projct/Exercise/anagrams.py
@@ -0,0 +1,40 @@
+
+from anagram_checker import AnagramChecker
+from string import ascii_letters as alph
+
+def is_valide_string(string):
+    for note in string:
+        if note not in alph:
+            return False
+    return True
+
+while True:
+    user_input1 = input("To Choose a word type 'S' (start), To Exit type 'E': ")
+    if user_input1 == "S" or user_input1 == "s":
+        while True:
+            user_input = input("type a word with no spaces, no number and no special charecters: ")
+            user_input = user_input.strip()
+            if is_valide_string(user_input):
+                user_anagram = AnagramChecker(user_input)
+                print(f"YOUR WORD: {user_anagram.word}")
+                if user_anagram.is_valid_word():
+                    print(f"this is a valid English word.")
+                    print_sring = ""
+                    for word in user_anagram.get_anagrams():
+                        if print_sring == "":
+                            print_sring+= word
+                        else:
+                            print_sring += f", {word}"
+                    print(f"Anagrams for your word: {print_sring}.")
+                    break
+            elif not is_valide_string(user_input):
+                print("not a valid INPUT")
+        break
+    elif user_input1 == "E" or user_input1 == "e":
+        print("Bye Bye!")
+        break
+
+
+
+
+    
