32bd761d793c4540ba47d40f3a75f36d9c9774ff
TeaTimeJack <amitjack14@gmail.com>
2025-09-01 22:11:27 +0300

    made changes to anagem mini project

--- week-2/day-5-mini-projct/Exercise/anagram_checker.py
+++ week-2/day-5-mini-projct/Exercise/anagram_checker.py
@@ -1,10 +1,14 @@
 
 class AnagramChecker:
     def __init__(self, user_word):
-        self.list_of_words = self.make_list_from_txt_file()
+        self.list_of_words = None
         self.word = self.make_word_capitalised(user_word)
-
+    
     @staticmethod
+    def make_word_capitalised(word):
+        cap_word = word.upper()
+        return cap_word
+    
     def make_list_from_txt_file():
         file_path = "week-2/day-5-mini-projct/Exercise/sowpods.txt"
         text_file_list=[]
@@ -13,32 +17,34 @@ class AnagramChecker:
         for line in lines:
             text_file_list.append(line.strip())
         return text_file_list
-    
-    @staticmethod
-    def make_word_capitalised(word):
-        cap_word = word.upper()
-        return cap_word
 
     def is_valid_word(self):
+        self.list_of_words =  self.make_list_from_txt_file()
         if self.word in self.list_of_words:
             return True
         else: return False
 
     def is_anagram(self, word2):
-        if len(self.word) == len(word2):
-            for letter in self.word:
-                if letter not in word2:
-                    return False
-            for letter in word2:
-                if letter not in self.word:
-                    return False    
-            return True 
+        if sorted(self.word) == sorted(word2):
+                return True
         else: return False
+
+        # if len(self.word) == len(word2):
+        #     for letter in self.word:
+        #         if letter not in word2:
+        #             return False
+        #     for letter in word2:
+        #         if letter not in self.word:
+        #             return False    
+        #     return True 
+        # else: return False
 
     def get_anagrams(self):
         if self.is_valid_word():
-            list_of_anagrams = []
+            list_of_anagrams = ()
             for word in self.list_of_words:
+                if len(word) != len(self.word):
+                    continue
                 if word == self.word:
                     continue
                 if self.is_anagram(word):
