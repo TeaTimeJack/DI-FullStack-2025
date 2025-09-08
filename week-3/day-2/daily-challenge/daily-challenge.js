dd1a21e495eb7d585bc2cdff0032448b266beb41
TeaTimeJack <amitjack14@gmail.com>
2025-09-08 23:11:38 +0300

    made background color frim class and not from arry

--- week-3/day-2/daily-challenge/daily-challenge.js
+++ week-3/day-2/daily-challenge/daily-challenge.js
@@ -1,13 +1,14 @@
-let listPlanets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
-let planetsColor = ["grey", "yellow", "green", "orange", "blanchedalmond", "palegoldenrod", "lightseagreen", "blue"]
+let planetsName = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+// let planetsColor = ["grey", "yellow", "green", "orange", "blanchedalmond", "palegoldenrod", "lightseagreen", "blue"]
 
 let mainSection = document.querySelector(".listPlanets");
 
-for (let i = 0; i < listPlanets.length; i++) {
+for (let i = 0; i < planetsName.length; i++) {
     planet = document.createElement("div");
     planet.setAttribute("class", "planet")
-    planet.style.backgroundColor = planetsColor[i]
+    planet.classList.add(planetsName[i])
+    // planet.style.backgroundColor = planetsColor[i]
     planet.style.margin = "5px"
-    planet.textContent = listPlanets[i]
+    planet.textContent = planetsName[i]
     mainSection.appendChild(planet);
 }
