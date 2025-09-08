8afdfd2744a1a1f493ddf5dbff3a997765fa480c
TeaTimeJack <amitjack14@gmail.com>
2025-09-08 18:07:15 +0300

    finnish ex week 3 day 2

--- week-3/day-1/daily-challenge/daily-challenge-not-bad.js
+++ week-3/day-1/daily-challenge/daily-challenge-not-bad.js
@@ -1,9 +1,9 @@
 
-let sentence = "This dinner is not that bad ! You cook well"
+let sentence = "This dinner is bad !"
 let wordNot = sentence.indexOf("not")
 let wordBad = sentence.indexOf("bad")
-cutIndex = wordBad-wordNot
-if (wordBad > wordNot){
+
+if (wordBad > wordNot && wordNot !== -1 && wordBad !== -1){
     let newSentence = sentence.slice(0,wordNot) + "good" + sentence.slice(wordBad+3)
     console.log(newSentence)
 }else{
