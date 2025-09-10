4dc1219c74abc5d0d1c006798c14253ea12329ee
TeaTimeJack <amitjack14@gmail.com>
2025-09-10 16:00:52 +0300

    finnish ex day 4 week 3

---
+++ week-3/day-4/Exercise/exercise1-XP.js
@@ -0,0 +1,74 @@
+// #1
+function funcOne() {
+    let a = 5;
+    if(a > 1) {
+        a = 3;
+    }
+    alert(`inside the funcOne function ${a}`);
+}
+
+// #1.1 - run in the console:
+funcOne()
+// #1.2 What will happen if the variable is declared 
+// with const instead of let ?
+// errOr - cant change a const value
+
+//#2
+ a = 0;
+function funcTwo() {
+    a = 5;
+}
+
+function funcThree() {
+    alert(`inside the funcThree function ${a}`);
+}
+
+// #2.1 - run in the console:
+funcThree()
+funcTwo()
+funcThree()
+// #2.2 What will happen if the variable is declared 
+// with const instead of let ?
+// errOr - cant change a const value
+
+
+//#3
+function funcFour() {
+    window.a = "hello";
+}
+
+
+function funcFive() {
+    alert(`inside the funcFive function ${a}`);
+}
+
+// #3.1 - run in the console:
+funcFour()
+funcFive()
+
+//#4
+ a = 1;
+function funcSix() {
+     a = "test";
+    alert(`inside the funcSix function ${a}`);
+}
+
+
+// #4.1 - run in the console:
+funcSix()
+// #4.2 What will happen if the variable is declared 
+// with const instead of let ?
+// errOr - cant change a const value
+
+//#5
+ a = 2;
+if (true) {
+     a = 5;
+    alert(`in the if block ${a}`);
+}
+alert(`outside of the if block ${a}`);
+
+// #5.1 - run the code in the console
+// #5.2 What will happen if the variable is declared 
+// with const instead of let ?
+// errOr - cant change a const value
