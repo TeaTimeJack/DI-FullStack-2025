00d82e0908d9a53a4d817bebdd40e836cf8b6f4d
TeaTimeJack <amitjack14@gmail.com>
2025-09-14 22:16:47 +0300

    finnished daily challenge day 1 week 4

---
+++ week-4/day-1/daily-challenge/daily-challenge-GO-Wildcats.js
@@ -0,0 +1,47 @@
+const gameInfo = [
+ {
+   username: "john",
+   team: "red",
+   score: 5,
+   items: ["ball", "book", "pen"]
+ },
+ {
+   username: "becky",
+   team: "blue",
+   score: 10,
+   items: ["tape", "backpack", "pen"]
+ },
+ {
+   username: "susy",
+   team: "red",
+   score: 55,
+   items: ["ball", "eraser", "pen"]
+ },
+ {
+   username: "tyson",
+   team: "green",
+   score: 1,
+   items: ["book", "pen"]
+ },
+];
+
+const usernameArray = []
+
+gameInfo.forEach(user =>{
+    usernameArray.push(`${user.username}!`)
+})
+console.log(usernameArray);
+
+const winners = []
+gameInfo.forEach(user =>{
+    if(user.score > 5){
+        winners.push(user.username)
+    }
+});
+console.log(winners);
+
+let theSum = gameInfo.reduce((sum , user) => {
+    sum += user.score
+    return sum
+},0);
+console.log(theSum);
