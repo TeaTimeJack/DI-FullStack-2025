99188553b51db46583049e8205b8ec56a1222686
TeaTimeJack <amitjack14@gmail.com>
2025-09-21 16:23:53 +0300

    updated the build folder

--- week-5/day-2/Daily-Challenge/build/unionTapye.js
+++ week-5/day-2/Daily-Challenge/build/unionTapye.js
@@ -1,3 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const allTypes = ["number", "string", "boolean", "object"];
+function validateUnionType(value, allowedTypes) {
+    for (let i = 0; i < allowedTypes.length; i++) {
+        const aType = allowedTypes[i];
+        if (typeof value === aType) {
+            return true;
+        }
+    }
+    return false;
+}
+;
+console.log(validateUnionType("dog", allTypes));
+console.log(validateUnionType(true, allTypes));
+console.log(validateUnionType(5, allTypes));
 //# sourceMappingURL=unionTapye.js.map
