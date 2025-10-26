6f06ff75d355e588598198ae42258555a74bc78f
TeaTimeJack <amitjack14@gmail.com>
2025-10-26 21:21:10 +0200

    finnished ex xp day 4 react

---
+++ week-7/day-4/exercises/ex-xp/src/App.jsx
@@ -0,0 +1,32 @@
+import UserFavoriteAnimals from './UserFavoriteAnimals'
+import Exercise from "./Exercise3"
+import './App.css'
+
+function App() {
+  const myelement = <h1>I Love JSX!</h1>;
+  const sum = 5+5;
+  const user = {
+  firstName: 'Bob',
+  lastName: 'Dylan',
+  favAnimals : ['Horse','Turtle','Elephant','Monkey']
+  };
+
+  return (
+    <>
+      {myelement}
+      <p>Hello World</p>
+      <p>React is {sum} times better with JSX</p>
+      <h3>{user.firstName}</h3>
+      <h3>{user.lastName}</h3>
+      <ul>
+        {user.favAnimals.map((item, index)=>{
+          return <UserFavoriteAnimals animal={item} key={index}/>
+        })}
+      </ul>
+      <Exercise />
+      
+    </>
+  )
+}
+
+export default App
---
+++ week-7/day-4/exercises/ex-xp/src/UserFavoriteAnimals.jsx
@@ -0,0 +1,7 @@
+
+const UserFavoriteAnimals = (props) =>{
+   const {animal} = props
+   return (<li>{animal}</li>)
+}
+
+export default UserFavoriteAnimals
